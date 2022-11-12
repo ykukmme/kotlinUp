b595c03d97c79e79f3c721a6a1521e874a78707a
ykukmme <rdijk021457@gmail.com>
2022-11-12 21:36:24 +0900

    wise_saying

---
+++ wise/app/src/main/java/fastcampus/aop/wise/MainActivity.kt
@@ -0,0 +1,95 @@
+package fastcampus.aop.wise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import androidx.viewpager2.widget.ViewPager2
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import org.json.JSONArray
+import org.json.JSONObject
+import kotlin.math.absoluteValue
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewPaser: ViewPager2 by lazy {
+        findViewById(R.id.viewPager)
+    }
+
+    private val progressBar: ProgressBar by lazy {
+        findViewById(R.id.progressBar)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+        initData()
+    }
+
+    private fun initViews() {
+        viewPaser.setPageTransformer { page, position ->
+            when {
+                position.absoluteValue >= 1.0F -> {
+                    page.alpha = 0F
+                }
+                position == 0F -> {
+                    page.alpha = 1F
+                }
+                else -> {
+                    page.alpha = 1F - 2 * position.absoluteValue
+                }
+            }
+        }
+    }
+
+    private fun initData() {
+        val remoteConfig = Firebase.remoteConfig
+        remoteConfig.setConfigSettingsAsync(
+            remoteConfigSettings {
+                minimumFetchIntervalInSeconds = 0
+            }
+        )
+
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            progressBar.visibility = View.GONE
+            if (it.isSuccessful) {
+                val quotes = parseQuotesJson(remoteConfig.getString("quotes"))
+                val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
+                displayQuotesPager(quotes, isNameRevealed)
+
+            }
+        }
+    }
+
+    private fun displayQuotesPager(quotes: List<Quote>, isnameRevealed: Boolean) {
+
+        val adapter = QuotesPagerAdapter(
+            quotes = quotes,
+            isnameRevealed
+        )
+        viewPaser.adapter = adapter
+        viewPaser.setCurrentItem(adapter.itemCount / 2, false)
+    }
+
+    private fun parseQuotesJson(json: String): List<Quote> {
+        val jsonArray = JSONArray(json)
+        var jsonList = emptyList<JSONObject>()
+
+        for (index in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(index)
+            jsonObject?.let {
+                jsonList = jsonList + it
+            }
+        }
+        return jsonList.map {
+            Quote(
+                quote = it.getString("quote"),
+                name = it.getString("name")
+            )
+        }
+    }
+}
---
+++ wise/app/src/main/java/fastcampus/aop/wise/Quote.kt
@@ -0,0 +1,6 @@
+package fastcampus.aop.wise
+
+data class Quote(
+    val quote: String,
+    val name: String
+)
