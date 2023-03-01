61e21c99653c9e42aad1f8c7d95eb24bdf4b01c2
ykukmme <rdijk021457@gmail.com>
2023-03-02 07:28:05 +0900

    number_count

---
+++ count_num/app/src/main/java/fastcampus/aop/count_num/MainActivity.kt
@@ -0,0 +1,11 @@
+package fastcampus.aop.count_num
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
